842093d0f6022597b779014b0587dea3cfca4ed3
Omprakash <omprakash.mopuri@improve10x.com>
2022-10-31 23:16:22 +0530

    added new activity

--- app/src/main/java/com/example/intrestcalculator/InterestCalculatorActivity.java
+++ app/src/main/java/com/example/intrestcalculator/InterestCalculatorActivity.java
@@ -2,7 +2,9 @@ package com.example.intrestcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InterestCalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class InterestCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interest_calculator);
+        handleInterestButton();
+    }
+
+    public void handleInterestButton() {
+        Button interestCalculatorBtn = findViewById(R.id.interest_calculator_btn);
+        interestCalculatorBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CalculaterActivity.class);
+            startActivity(intent);
+        });
     }
 }
